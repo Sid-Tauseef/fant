2bfab0edecbc2c25fdc233a0707d7f6d998221bb
Tauseeb Ahmed Siddiqui <122994540+Sid-Tauseef@users.noreply.github.com>
2025-07-24 11:58:37 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -7,7 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBsPOswXR4W4n5J0NVanvvdv9j1D_bK_pk",
+  apiKey: "AIzaSyBsPOswXR4W4n5J0NVanvvdv9j1D_bK_pksdasdjfka",
   authDomain: "sidifysolutions.firebaseapp.com",
   projectId: "sidifysolutions",
   storageBucket: "sidifysolutions.firebasestorage.app",
